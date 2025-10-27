19534282e81131f369f5cbd6bb422c776c7428db
kllk15656 <kristinekennedy20@gmail.com>
2025-10-27 16:43:33 +0000

    uopdate

--- react-markdown-svelte/src/components/Routes.tsx
+++ react-markdown-svelte/src/components/Routes.tsx
@@ -3,7 +3,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import FigmaTutorial from "./Tutorial";
 import Notes from "./Notes";
-import PresentationPage from "./Presentation"; 
+import PresentationPage from "./Presentation";
 import "./Home.css";
 
 // Home page component
@@ -17,23 +17,18 @@ const Home: React.FC = () => (
     <div className="fun-fact-box">
       <h3>💡 Fun Fact</h3>
       <p>The Welcome message was created by the Figma design Feature!</p>
-      <img
-        src="/Design.png"
-        alt="Fun fact illustration"
-        className="fun-fact-image"
-      />
+      <img src="/Design.png" alt="Fun fact illustration" className="fun-fact-image" />
     </div>
   </div>
 );
 
-// Main App Routes
 const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Presentation" element={<PresentationPage />} /> 
+        <Route path="/Presentation" element={<PresentationPage />} />
         <Route path="/Tutorial" element={<FigmaTutorial />} />
         <Route path="/Supporting" element={<Notes />} />
       </Routes>
